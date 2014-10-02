331e590e063200f2ac430a5d6bad16917a71d567
RWOverdijk <r.w.overdijk@gmail.com>
2014-10-02 19:04:00 +0200

    stricter

--- config/blueprints.js
+++ config/blueprints.js
@@ -57,7 +57,7 @@ module.exports.blueprints = {
   // if you'd like to continue to use controller/action autorouting in a production deployment,
   // you must take great care not to inadvertently expose unsafe/unintentional controller logic
   // to GET requests.
-  actions: true,
+  actions: false,
 
 
 
@@ -86,38 +86,8 @@ module.exports.blueprints = {
   // for the controller's`find`, `create`, `update`, and `destroy` actions.
   //
   // `shortcuts` are enabled by default, but should be disabled in production.
-  shortcuts: true,
-
-
-
-  // An optional mount path for all blueprint routes on a controller, including `rest`,
-  // `actions`, and `shortcuts`.  This allows you to take advantage of blueprint routing,
-  // even if you need to namespace your API methods.
-  //
-  // * (NOTE: This only applies to blueprint autoroutes, not manual routes from `sails.config.routes`)
-  //
-  // For example, `prefix: '/api/v2'` would make the following REST blueprint routes
-  // for a FooController:
-  //
-  // `GET /api/v2/foo/:id?`
-  // `POST /api/v2/foo`
-  // `PUT /api/v2/foo/:id`
-  // `DELETE /api/v2/foo/:id`
-  //
-  // By default, no prefix is used.
+  shortcuts: false,
   prefix: '',
-
-
-
-  // Whether to pluralize controller names in blueprint routes.
-  //
-  // (NOTE: This only applies to blueprint autoroutes, not manual routes from `sails.config.routes`)
-  //
-  // For example, REST blueprints for `FooController` with `pluralize` enabled:
-  // GET    /foos/:id?
-  // POST   /foos
-  // PUT    /foos/:id?
-  // DELETE /foos/:id?
   pluralize: false
 
 };
